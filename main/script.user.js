b629f0f0fc757ec76a95c3f44a0e1bf51a1107e2
FlJesusLorenzo <jesus.lorenzo@factorlibre.com>
2025-11-12 14:26:37 +0100

    Update script.user.js

--- main/script.user.js
+++ main/script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Google Meet Imputación automática
 // @namespace    http://tampermonkey.net/
-// @version      2.0.0
+// @version      2.0.1
 // @description  Registra el tiempo del meet y genera la imputacion automaticamente
 // @author       Jesus Lorenzo
 // @grant        GM_setValue
